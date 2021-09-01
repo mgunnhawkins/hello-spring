f2169951b85c4015f200dd5a78c9d917e1ee730d
Melanie Gunn <mgunn589@gmail.com>
2021-09-01 17:17:08 -0500

    Exercise complete with minimal styling

--- SpringBoot/hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ SpringBoot/hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -6,50 +6,66 @@ import org.springframework.web.bind.annotation.*;
 @Controller //tells SpringBoot that this class represents a web controller. AKA there are methods in this class
 // that are setup to handle HTTP requests.
 @ResponseBody
-@RequestMapping
+@RequestMapping("hello")//all methods are at backslash hello
 public class HelloController {
 
-    //lives at /hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye(){
-        return "Goodbye, Spring!";
-    }
-    @GetMapping("hello")
-    public String createMessage(String name, String language){
-        String greeting = "";
-        if (language.equalsIgnoreCase("English")) {
-            greeting = "Hello";
-        } else if (language.equalsIgnoreCase("French")){
-            greeting = "Bonjour";
-        }
 
-        return language + ", " + name + "!";
-    }
+
 
     // Handles request of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}) //handles both GET and POST
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST}) //handles both GET and POST
     // requests
-    public String helloWithQueryParam(@RequestParam String name) {
+    public String gatherInfo(@RequestParam String name, @RequestParam String language){
+        return createMessage(name,language);
+    }
+    public String createMessage(String name, String language) {
+        String greeting = "";
+        if (language.equalsIgnoreCase("english")) {
+            greeting = "Hello";
+        } else if (language.equalsIgnoreCase("french")){
+            greeting = "Bonjour";
+        } else if (language.equalsIgnoreCase("spanish")) {
+            greeting = "Hola";
+        }else if (language.equalsIgnoreCase("hebrew")) {
+            greeting = "Shalom";
+        }else if (language.equalsIgnoreCase("italian")) {
+            greeting = "Ciao";
+        }
 
-        return "Hello, " + name + "!"; //dynamic response
+        return greeting + ", " + name + "!";
     }
 
-    // Handles request of the form /hello/LaunchCode
-    @GetMapping("{name}") //handles GET requests
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello, " + name + "!";
-    }
 
-
-    @GetMapping("form")
+    @GetMapping("form") //method to display form
     public String helloForm(){
         return "<html>" +
-                "<body>" +
-                "<form action='/hello' method='get'>" + //submit a request to /hello(method we created with queryParam)
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet me!'>" +
-                "</form>" +
-                "</body>" +
+                    "<style>" +
+                        "input{" +
+                            "border: solid #A9A9A9 2px;" +
+                            "border-radius: 10px;" +
+                            "padding: 5px;" +
+                            "margin: 10px;" +
+                        "}" +
+                        "select{" +
+                        "border: solid #A9A9A9 2px;" +
+                        "border-radius: 10px;" +
+                        "padding: 5px;" +
+                        "margin: 10px;" +
+                        "}" +
+                    "</style>" +
+                    "<body>" +
+                        "<form action='hello'>" + //submit a request to /hello(method we created with queryParam)
+                            "<input type='text' name='name'>" + //for text in a form
+                            "<select name='language' id='language'>" +
+                                "<option value='english'>English</option>" +
+                                "<option value='french'>French</option>" +
+                                "<option value='spanish'>Spanish</option>" +
+                                "<option value='hebrew'>Hebrew</option>" +
+                                "<option value='italian'>Italian</option>" +
+                            "</select>" +
+                            "<input type='submit' value='Greet me!'>" + //creates input button
+                        "</form>" +
+                "   </body>" +
                 "</html>";
     }
 
